6a8859aeba335077344d26b6fd98557d081aef10
Wilhelm Kutah <wilhelmkutah@Wilhelms-MacBook-Pro.local>
2026-01-31 00:13:44 +0000

    Debug: add test route

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+            <h1>Deployment Test Successful!</h1>
+            <p>If you can see this, the routing system is working.</p>
+        </div>
+    );
+}
